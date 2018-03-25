d892f284f4063329f80f44ce489ab2f1f3573b15
Patrick Lam <zidizei@gmail.com>
2018-03-25 15:59:52 +0200

    move deprecation check to seperate function

--- lib/env.sh
+++ lib/env.sh
@@ -14,16 +14,18 @@ export_env_dir() {
 create_build_env() {
     export ORIGINAL_ENV=${NODE_ENV:-production}
 
-    if [ -z "$TSC_CUSTOM_FILE" ]; then
-        echo "Warning: Please use TSC_CONFIG to set the path to a custom tsconfig.json for compilation."
-    fi
     export TSC_CUSTOM_FILE=${TSC_CUSTOM_FILE:-"$BUILD_DIR/tsconfig.json"}
     export TSC_CONFIG=${TSC_CONFIG:-$TSC_CUSTOM_FILE}
 
-    echo "Using configuration at $TSC_CONFIG"
-
     export NODE_ENV=${TSC_BUILD_ENV:-$NODE_ENV}
     export NODE_VERBOSE=${NODE_VERBOSE:-false}
+}
+
+deprecate_tsc_custom_file() {
+    if [ ! -z "$TSC_CUSTOM_FILE" ]; then
+        echo "[warn] Found TSC_CUSTOM_FILE environment variable."
+        echo "[warn] Please use TSC_CONFIG to set the path to a custom tsconfig.json for compilation."
+    fi
 }
 
 exit_build_env() {
